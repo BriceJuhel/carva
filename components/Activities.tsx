b14289092990d4ca1824d0fe2bd83921f81c2354
Brice JUHEL <juhel.brice@gmail.com>
2023-10-02 21:56:25 +0200

    ajustement séparation en version PC

--- components/Activities.tsx
+++ components/Activities.tsx
@@ -21,7 +21,7 @@ const Activities = () => {
           </div>          
         </div>
 
-        <div className="mx-auto mt-4 grid w-12 max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-blue-900 pt-4 lg:mx-0 lg:max-w-none md:grid-cols-2"></div>
+        <div className="mx-auto mt-4 grid w-12 max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-blue-900 pt-4 lg:max-w-none md:grid-cols-2"></div>
 
         <div className="">
         <div className="flex justify-center">
@@ -36,7 +36,7 @@ const Activities = () => {
           </div>          
         </div>
 
-        <div className="mx-auto mt-4 grid w-12 max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-blue-900 pt-4 lg:mx-0 lg:max-w-none md:grid-cols-2"></div>
+        <div className="mx-auto mt-4 grid w-12 max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-blue-900 pt-4 lg:max-w-none md:grid-cols-2"></div>
 
         <div className="">
         <div className="flex justify-center">
@@ -50,7 +50,7 @@ const Activities = () => {
           </div>          
         </div>
 
-        <div className="mx-auto mt-4 grid w-12 max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-blue-900 pt-4 lg:mx-0 lg:max-w-none md:grid-cols-2"></div>
+        <div className="mx-auto mt-4 grid w-12 max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-blue-900 pt-4 lg:max-w-none md:grid-cols-2"></div>
 
         <div className="">
           <div className="flex justify-center">
@@ -65,7 +65,7 @@ const Activities = () => {
           </div>          
         </div>
 
-        <div className="mx-auto mt-4 grid w-12 max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-blue-900 pt-4 lg:mx-0 lg:max-w-none md:grid-cols-2"></div>
+        <div className="mx-auto mt-4 grid w-12 max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-blue-900 pt-4 lg:max-w-none md:grid-cols-2"></div>
 
       <div className="">
         <div className="flex justify-center">
@@ -81,7 +81,7 @@ const Activities = () => {
         </div>          
       </div>
 
-      <div className="mx-auto mt-8 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-4 lg:mx-0 lg:max-w-none md:grid-cols-2"></div>
+      <div className="mx-auto mt-8 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-4 lg:max-w-none md:grid-cols-2"></div>
 
       </section>
 
