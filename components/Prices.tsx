e1c5e22a864513c18eb06fca2580285fff3c1de9
Brice JUHEL <juhel.brice@gmail.com>
2023-10-04 16:19:46 +0200

    maj des tarifs

--- components/Prices.tsx
+++ components/Prices.tsx
@@ -139,7 +139,7 @@ export default function Prices() {
               ))}
             </ul>
           </div>
-          <div className="-mt-2 p-2 lg:mt-10 lg:w-full lg:max-w-md lg:flex-shrink-0 lg:items-center">
+          <div className="-mt-2 p-2 lg:mt-2 lg:w-full lg:max-w-md lg:flex-shrink-0 lg:items-center">
             <div className="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
               <div className="mx-auto max-w-xs px-8">
                   <p className="mt-6 flex items-baseline justify-center gap-x-2">
@@ -176,10 +176,10 @@ export default function Prices() {
               ))}
             </ul>
           </div>
-          <div className="-mt-2 p-2 lg:mt-10 lg:w-full lg:max-w-md lg:flex-shrink-0 lg:items-center">
+          <div className="-mt-2 p-2 lg:mt-2 lg:w-full lg:max-w-md lg:flex-shrink-0 lg:items-center">
             <div className="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
               <div className="mx-auto max-w-xs px-8">
-                  <p className="text-base font-semibold text-gray-600 mt-2">Sur devis suivant le type de recherche du véhicule et localisation (exemple : 349€ TTC pour une recherche et inspection d&apos;un véhicule à 50km maximum autour de La Chevrolière)</p>
+                  <p className="text-base font-semibold text-gray-600 mt-2">Sur devis suivant le type de recherche du véhicule et de la localisation (exemple : 349€ TTC pour une recherche et inspection d&apos;un véhicule à 50km maximum autour de La Chevrolière)</p>
                 <a
                   href="#"
                   className="mt-10 block w-full rounded-md bg-orange-400 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
