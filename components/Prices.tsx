a6d280064e971c47787371070d17fe7c86c7ac02
Brice JUHEL <juhel.brice@gmail.com>
2023-10-04 16:27:58 +0200

    maj des tarifs

--- components/Prices.tsx
+++ components/Prices.tsx
@@ -1,4 +1,7 @@
+"use client" // this is a client component
 import { CheckIcon } from '@heroicons/react/20/solid'
+import { Link } from "react-scroll/modules"
+import React from "react"
 
 const includedOne = [
   'Vidange huile moteur',
@@ -52,12 +55,13 @@ export default function Prices() {
                   <span className="text-5xl font-bold tracking-tight text-gray-900">70€ TTC</span>
                   <span className="font-semibold leading-6 tracking-wide text-gray-600">(58,33€ HT)</span>
                 </p>
-                <a
-                  href="#"
-                  className="mt-10 block w-full rounded-md bg-orange-400 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Contactez-moi
-                </a>
+                <Link to="contact" 
+                  spy={true}
+                  smooth={true}
+                  offset={-120}
+                  duration={600}>
+                  <button className="mt-10 block w-full rounded-md bg-orange-400 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Prendre rendez-vous</button>
+                </Link>
               </div>
             </div>
           </div>
@@ -73,12 +77,13 @@ export default function Prices() {
                   <span className="text-3xl font-bold tracking-tight text-gray-900">1,80€ TTC/km</span>
                 </p>
                 <p className="text-base font-semibold text-gray-600 mt-2">Minimum de 90€ TTC, soit 50km</p>
-                <a
-                  href="#"
-                  className="mt-10 block w-full rounded-md bg-orange-400 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Contactez-moi
-                </a>
+                <Link to="contact" 
+                  spy={true}
+                  smooth={true}
+                  offset={-120}
+                  duration={600}>
+                  <button className="mt-10 block w-full rounded-md bg-orange-400 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Contactez-moi</button>
+                </Link>
               </div>
             </div>
           </div>
@@ -109,12 +114,13 @@ export default function Prices() {
                   <p className="mt-6 flex items-baseline justify-center gap-x-2">
                   <span className="text-5xl font-bold tracking-tight text-gray-900">149€ TTC</span>
                 </p>
-                <a
-                  href="#"
-                  className="mt-10 block w-full rounded-md bg-orange-400 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Contactez-moi
-                </a>
+                <Link to="contact" 
+                  spy={true}
+                  smooth={true}
+                  offset={-120}
+                  duration={600}>
+                  <button className="mt-10 block w-full rounded-md bg-orange-400 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Prendre rendez-vous</button>
+                </Link>
               </div>
             </div>
           </div>
@@ -146,12 +152,13 @@ export default function Prices() {
                   <span className="text-5xl font-bold tracking-tight text-gray-900">4%</span>
                   </p>
                   <p className="text-base font-semibold text-gray-600 mt-2">du prix de vente du véhicule une fois la vente conclue (minimum 390€ TTC)</p>
-                <a
-                  href="#"
-                  className="mt-10 block w-full rounded-md bg-orange-400 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Contactez-moi
-                </a>
+                  <Link to="contact" 
+                  spy={true}
+                  smooth={true}
+                  offset={-120}
+                  duration={600}>
+                  <button className="mt-10 block w-full rounded-md bg-orange-400 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Contactez-moi</button>
+                </Link>
               </div>
             </div>
           </div>
@@ -180,12 +187,13 @@ export default function Prices() {
             <div className="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
               <div className="mx-auto max-w-xs px-8">
                   <p className="text-base font-semibold text-gray-600 mt-2">Sur devis suivant le type de recherche du véhicule et de la localisation (exemple : 349€ TTC pour une recherche et inspection d&apos;un véhicule à 50km maximum autour de La Chevrolière)</p>
-                <a
-                  href="#"
-                  className="mt-10 block w-full rounded-md bg-orange-400 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Demandez un devis
-                </a>
+                  <Link to="contact" 
+                  spy={true}
+                  smooth={true}
+                  offset={-120}
+                  duration={600}>
+                  <button className="mt-10 block w-full rounded-md bg-orange-400 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Demandez un devis</button>
+                </Link>
               </div>
             </div>
           </div>
