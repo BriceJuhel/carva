fe3dbe1e0b7a7b3ed73e44d5114d6420062ac03a
Brice JUHEL <juhel.brice@gmail.com>
2023-10-13 13:15:43 +0200

    maj formulaire de contact

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,8 +1,62 @@
+import { useState } from 'react';
+
 function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(' ');
 }
 
 export default function Contact() {
+  const [formData, setFormData] = useState({
+    lastName: '',
+    firstName: '',
+    email: '',
+    phoneNumber: '',
+    recontact: '',
+    infos: '',
+    message: ''
+  });
+
+  const [successMessage, setSuccessMessage] = useState<string | null>(null);
+
+  const handleChange = (event: { target: { name: any; value: any; }; }) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = async (event: { preventDefault: () => void; target: any; }) => {
+    event.preventDefault();
+
+    const form = event.target;
+    const formData = new FormData(form);
+
+    try {
+      const response = await fetch('/api/sendEmail', {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (response.ok) {
+        setSuccessMessage('Votre demande a bien été envoyée');
+        form.reset(); // Réinitialiser le formulaire
+        setFormData({
+          lastName: '',
+          firstName: '',
+          email: '',
+          phoneNumber: '',
+          recontact: '',
+          infos: '',
+          message: ''
+        });
+      } else {
+        setSuccessMessage('Une erreur s\'est produite lors de l\'envoi de l\'e-mail');
+      }
+    } catch (error) {
+      console.error(error);
+      setSuccessMessage('Une erreur s\'est produite lors de l\'envoi de l\'e-mail');
+    }
+  };
 
   return (
     <section className="isolate bg-white px-6 sm:py-8 lg:px-8 mt-6 sm:mt-2" id="contact">
@@ -16,21 +70,20 @@ export default function Contact() {
         <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl underline">Contact</h2>
       </div>
       <div className='font-semibold text-gray-600 mt-4 text-center flex justify-center'>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5 mr-3">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
-                </svg>
-                <p>  06.06.06.06.06</p>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5 mr-3">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+        </svg>
+        <p>  06.06.06.06.06</p>
       </div>
       <div className='font-semibold text-gray-600 mt-2 text-center flex justify-center ml-4'>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5 mr-1">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
-                </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5 mr-1">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+        </svg>
 
-                <p>  carva@email.com</p>
+        <p>  carva@email.com</p>
       </div>
-      <form action="../api/sendEmail" method="POST" className="mx-auto mt-12 max-w-xl sm:mt-12">
+      <form onSubmit={handleSubmit} className="mx-auto mt-12 max-w-xl sm:mt-12">
         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-          
           <div>
             <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-gray-900">
               Nom
@@ -42,10 +95,11 @@ export default function Contact() {
                 id="last-name"
                 autoComplete="family-name"
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-400 sm:text-sm sm:leading-6"
+                value={formData.lastName}
+                onChange={handleChange}
               />
             </div>
           </div>
-
           <div>
             <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">
               Prénom
@@ -57,6 +111,8 @@ export default function Contact() {
                 id="first-name"
                 autoComplete="given-name"
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-400 sm:text-sm sm:leading-6"
+                value={formData.firstName}
+                onChange={handleChange}
               />
             </div>
           </div>
@@ -72,6 +128,8 @@ export default function Contact() {
                 autoComplete="email"
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-400 sm:text-sm sm:leading-6"
                 placeholder='exemple@email.com'
+                value={formData.email}
+                onChange={handleChange}
               />
             </div>
           </div>
@@ -87,6 +145,8 @@ export default function Contact() {
                 autoComplete="tel"
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-400 sm:text-sm sm:leading-6"
                 placeholder='01 23 45 67 89'
+                value={formData.phoneNumber}
+                onChange={handleChange}
               />
             </div>
           </div>
@@ -100,8 +160,9 @@ export default function Contact() {
                 id="recontact"
                 rows={3}
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-400 sm:text-sm sm:leading-6"
-                defaultValue={''}
                 placeholder='Exemple : par téléphone, par mail, le matin, le soir...'
+                value={formData.recontact}
+                onChange={handleChange}
               />
             </div>
           </div>
@@ -115,8 +176,9 @@ export default function Contact() {
                 id="infos"
                 rows={3}
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-400 sm:text-sm sm:leading-6"
-                defaultValue={''}
                 placeholder='Immatriculation, kilométrage...'
+                value={formData.infos}
+                onChange={handleChange}
               />
             </div>
           </div>
@@ -130,7 +192,8 @@ export default function Contact() {
                 id="message"
                 rows={12}
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-400 sm:text-sm sm:leading-6"
-                defaultValue={''}
+                value={formData.message}
+                onChange={handleChange}
               />
             </div>
           </div>
@@ -144,7 +207,12 @@ export default function Contact() {
           </button>
         </div>
       </form>
+      {successMessage && (
+        <div className="text-green-500 mt-4 text-center font-bold text-xl">
+          {successMessage}
+        </div>
+      )}
       <div className="mx-auto mt-8 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-4 lg:max-w-none md:grid-cols-2"></div>
     </section>
-  )
+  );
 }
