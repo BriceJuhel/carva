e37245bb2c467f61743a5238d83dcbb0a0c0a3ab
Brice JUHEL <juhel.brice@gmail.com>
2023-10-17 11:57:19 +0200

    modif prix révision

--- components/Prices.tsx
+++ components/Prices.tsx
@@ -111,8 +111,9 @@ export default function Prices() {
           <div className="-mt-2 p-2 lg:mt-10 lg:w-full lg:max-w-md lg:flex-shrink-0 lg:items-center">
             <div className="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
               <div className="mx-auto max-w-xs px-8">
-                  <p className="mt-6 flex items-baseline justify-center gap-x-2">
-                  <span className="text-5xl font-bold tracking-tight text-gray-900">149€ TTC</span>
+                  <p className="text-base font-semibold text-gray-600 mt-2">À partir de</p>
+                  <p className="mt-6 flex items-baseline justify-center gap-x-2">                    
+                  <span className="text-5xl font-bold tracking-tight text-gray-900">119€ TTC</span>
                 </p>
                 <Link to="contact" 
                   spy={true}
